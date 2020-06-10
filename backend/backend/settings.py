ad5e15a7d49b90c30bce93b0f84c04401ae01869
Yonggiee <fongyongjie27@gmail.com>
2020-06-10 20:55:50 +0800

    add serializers for tags

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'taggit',
+    'taggit_serializer',
     'expense',
     'trip'
 ]
--- backend/trip/serializers.py
+++ backend/trip/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
+from taggit_serializer.serializers import (TagListSerializerField,
+                                           TaggitSerializer)
 
 from user.serializers import UserSerializer
 from .models import Trip
 
-class TripSerializer(serializers.ModelSerializer):
+class TripSerializer(TaggitSerializer, serializers.ModelSerializer):
     members = UserSerializer(read_only=True, many=True)
+    tags = TagListSerializerField()
 
     class Meta:
         model = Trip
-        fields = ('id', 'name', 'desc', 'members', 'trip_start', 'trip_end')
+        fields = ('id', 'name', 'desc', 'members', 'trip_start', 'trip_end', 'tags')
